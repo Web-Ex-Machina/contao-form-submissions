693819228db98ff11ddbce34a40ea35cfe724ad6
Jeff <quentin@webexmachina.fr>
2018-06-27 16:52:13 +0200

    + added a hook on page's leaving (other than submit) that send the unregistered data to server * compute the fields value on keyup and copy-paste, not on blur

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -29,6 +29,7 @@ class wemForm{
 		wemForm.formID = formID;
 		wemForm.el = $(form);
 		wemForm.logs = [];
+		wemForm.fields = {};
 
 		wemForm.el.find('input, select, textarea').bind('focus', function(e){
 			wemForm.logs.push({
@@ -43,10 +44,16 @@ class wemForm{
 				,"type": "blur"
 				,"log": "L'utilisateur est sorti du champ : "+$(this).attr('name')+" avec la valeur : "+$(this).val()
 			});
+		})
+		.bind('keyup change',function(e){
+			// register fields values from user inputs
+			wemForm.fields[parseInt($(this).attr('id').replace('ctrl_', ''))] = $(this).val();
 		});
 
 		wemForm.el.bind('submit', function(e){
 			e.preventDefault();
+			// prevent the aborted submission storage
+			window.onbeforeunload = null;
 			// prevent any additional submission while processing
 			wemForm.el.find('.submit').bind('click',function(e){e.preventDefault()});
 
@@ -57,9 +64,32 @@ class wemForm{
 			});
 
 			wemForm.el.find('input[name="wem_tracker_logs"]').first().attr('value', JSON.stringify(wemForm.logs));
-			
+
 			wemForm.el.unbind('submit');
 			wemForm.el.submit();
 		});
+		wemForm.onAbort = window.onbeforeunload = function(e){
+			if(Object.keys(wemForm.fields).length || wemForm.logs.length){
+				$.ajax({
+					type: 'POST',
+					async: false,
+					url: window.location.pathname,
+					data: {
+						"TL_AJAX": 1
+	          ,"REQUEST_TOKEN": wemForm.el.find('input[name="REQUEST_TOKEN"]').first().attr('value')
+	          ,"module": "wem-contao-form-submissions"
+	          ,"action": "abortSubmission"
+	          ,"form": wemForm.formID
+	          ,"fields":wemForm.fields
+	          ,"logs":wemForm.logs
+					}
+				}).done(function(data){
+					// console.log(data.status, data);
+				});
+			}
+		}
 	}
 }
+
+
+
